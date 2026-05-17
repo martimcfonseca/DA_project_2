e7aea9cd23550559a13a7ecccf4320fabb60219d
Cristiano Silva <cristianojorge08@gmail.com>
2026-05-17 13:04:16 +0100

    fix main mod batch

--- src/main.cpp
+++ src/main.cpp
@@ -103,9 +103,9 @@ int main(int argc, char* argv[]) {
 
 
      if (argc == 5 && std::string(argv[1]) == "-b") {   //Modo batch "-b"
-         std::string rangesFile = argv[1];
-         std::string registersFile = argv[2];
-         std::string outputFile = argv[3];
+         std::string rangesFile = argv[2];
+         std::string registersFile = argv[3];
+         std::string outputFile = argv[4];
          try {
              auto ranges = Parser::parseRanges(rangesFile);
              int numRegistos = 0;
